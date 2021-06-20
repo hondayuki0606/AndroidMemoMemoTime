3991452a92c39ff01f25f24dd8a5bcad6a68ef3b
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-06-21 08:46:43 +0900

    add checkpermission

--- app/src/main/java/com/example/memomemotime/MainActivity.kt
+++ app/src/main/java/com/example/memomemotime/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.memomemotime
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 画面をスリープにしない
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        val mapFragment = supportFragmentManager.findDragmentById(this)
+    }
+    
+    override fun onMapReady(googleMap: GoogleMap){
+        mMap = googleMap
+
+    }
+    
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION))
+            == PackageManager.PERMISSION_GRANTED){
+                
+        } else {
+            
+        }
     }
 }
