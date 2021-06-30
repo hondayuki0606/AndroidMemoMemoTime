b3ddc65d27698d30b5ecf6e7dba48164ce8ec0dc
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-06-30 09:02:16 +0900

    lastLocationの実装

--- app/src/main/java/com/example/memomemotime/MainActivity.kt
+++ app/src/main/java/com/example/memomemotime/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.memomemotime
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 //import androidx.appcompat.app.ActivityCompat
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -16,6 +20,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
 
+    private lateinit var fusedLacoationProviderClient : FusedLocationProviderClient
+    private lateinit var lastLocation : Location
+    private var locationCallback : LocationCallback? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +31,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map)
         mapFragment.getMapAsync(this)
+        fusedLacoationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
     
     override fun onMapReady(googleMap: GoogleMap){
@@ -67,6 +76,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun myLocationEnable(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackagerManager.PERMISSION_GRANTED){
             mMap.isMyLocationEnabled = true
+            val locationResult = LocationRequest().apply {
+                interval = 10000
+                fastesInterval 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+            locationCallback = object: LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?){
+                    if(locationResult?.lastLocation != null) {
+                        lastLocation = locationResult?.lastLocation
+                        val currentLatLng = LatLng(lastLocation.latitude,lastLocation.longitude)
+                        mMap.moceCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                        textView
+                        
+                    }
+                }
+            }
         }
     }
 
